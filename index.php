c424aea1fba31c2c42a66a7cde07ccfe5ab30285
BlackPhantom <junii.AR35@gmail.com>
2025-05-19 20:56:00 -0300

    sd1314

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Encuestas Integración V6</title>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css"> <!-- Asegúrate que esta ruta sea correcta -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
