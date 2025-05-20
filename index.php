a8ce5878372e0adb9c6a6a6f85ca4f53f9542f40
moto <guillermoillanes233@gmail.com>
2025-05-19 21:30:30 -0300

    holi

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <title>Encuestas Integración V6</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css"> <!-- Asegúrate que esta ruta sea correcta -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
@@ -9,6 +10,7 @@
 <?php
 session_start();
 error_reporting(0);
+//holaamigodeyotube
 function obtenerDatosEmpresa($id) {
     $archivo = fopen("csvfiles/empresas.csv", "r");
     if ($archivo !== FALSE) {
